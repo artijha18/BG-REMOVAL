d39f38c0fab32a7b1cee854539aa7dcd4c948084
Aishraj30 <aishubamoriya@gmail.com>
2025-11-29 00:38:36 +0530

    aa

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173' })); 
+app.use(cors({ origin: process.env.CLIENT_URL || 'https://bg-removal-topaz-six.vercel.app/' })); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
